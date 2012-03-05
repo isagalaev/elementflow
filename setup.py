7891d376fe28ddf258dd38403cf9afbb2220e8bf
Ivan Sagalaev <maniac@softwaremaniacs.org>
2012-03-04 22:42:26 -0800

    Moved code under src/

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     version = '0.3.0',
     author = 'Ivan Sagalaev',
     author_email = 'Maniac@SoftwareManiacs.Org',
+    package_dir = {'': 'src'},
     py_modules = ['elementflow'],
     url = 'https://launchpad.net/elementflow',
     license = 'LICENSE.txt',
