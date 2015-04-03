6e4b3d675806227d5482e9e5fd169b1b4ee851f8
Ivan Sagalaev <maniac@softwaremaniacs.org>
2015-04-03 13:18:14 -0700

    Modernized and simplified package structure.

--- setup.py
+++ setup.py
@@ -1,17 +1,23 @@
-# -*- coding:utf-8 -*-
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name = 'elementflow',
     version = '0.4.2',
     author = 'Ivan Sagalaev',
-    author_email = 'Maniac@SoftwareManiacs.Org',
-    package_dir = {'': 'src'},
-    py_modules = ['elementflow'],
-    install_requires=['six'],
+    author_email = 'maniac@softwaremaniacs.org',
     url = 'https://github.com/isagalaev/elementflow',
-    license = 'LICENSE.txt',
-    description = 'Python library for generating XML as a stream without first building a tree in memory.',
+    license = 'BSD',
+    description = 'Python library for generating XML as a stream without building a tree in memory.',
     long_description = open('README.md').read(),
-    test_suite='runtests.runtests',
+
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+
+    install_requires = ['six'],
+    py_modules = ['elementflow'],
 )
